be31efa2b2ef5e7d637c12c0fe35f0d22900c8ab
NETSTREAM-KE <LEONARDKIBOR92@GMAIL.com>
2025-04-12 20:03:24 +0300

    Changed the nesting of the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import Home from './pages/homepage';
-import Projects from './pages/projects';
-import Services from './pages/services'
-import About from './pages/about'
-import Contact from './pages/contact/Index';
-import MainLayout from './layouts/MainLayout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/homepage";
+import Projects from "./pages/projects";
+import Services from "./pages/services";
+import About from "./pages/about";
+import Contact from "./pages/contact/Index";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
-
-
   return (
-    <Router>
     <Routes>
       <Route element={<MainLayout />}>
-
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
-      
     </Routes>
-  </Router>
-  )
+  );
 }
 
-export default App
+export default App;
